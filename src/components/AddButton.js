25e9de10a051dec4c4e24ab91f223087696c14d7
stéphane <darksh3ll.js@gmail.com>
2021-01-05 21:46:32 +0100

    style:(#)(component) : styled Button
    css integration with UI material

--- src/components/AddButton.js
+++ src/components/AddButton.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
+import { withStyles } from '@material-ui/core/styles';
+import { deepOrange } from '@material-ui/core/colors';
 
+const ColorButton = withStyles((theme) => ({
+  root: {
+    color: theme.palette.getContrastText(deepOrange[500]),
+    backgroundColor: deepOrange[500],
+    borderRadius:'50%',
+    padding:20,
+    '&:hover': {
+      backgroundColor: deepOrange[700],
+    },
+  },
+}))(Button);
 
 const AddButton  = () => {
   return (
-    <div>
-      <h1>CallToActionAdd</h1>
-    </div>
+    <ColorButton>
+      <AddIcon fontSize="large"/>
+    </ColorButton>
   );
 }
 
